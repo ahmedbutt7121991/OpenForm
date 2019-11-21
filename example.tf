43e5b152dce43fa82e670c0ef0c858e7be421367
ahmedbutt7121991 <ahmed.butt91@gmail.com>
2019-11-21 04:51:49 -0600

    simple creation completed

--- example.tf
+++ example.tf
@@ -1,30 +1,18 @@
 # Configure the OpenStack Provider
 # Authenticating the Openstack with Admin User Credentials
-provider "openstack" {
-  user_name                     = "admin"
-  tenant_name                   = "admin"
-  password                      = "w77JAyu7Aj4gFKhneu9y88sd9"
-  auth_url                      = "http://100.67.60.61:5000//v3"
-  user_domain_name              = "Default"
-  project_domain_name           = "Default"
-  disable_no_cache_header       = true
-}
 
 # This will Create a ROUTER NAMED terraform_router and the resource name will be router_1
 # External network id is for PUBLIC NETWORK
 resource "openstack_networking_router_v2" "router_1" {
-  name                = "terraform_router"
+  name                = "router_1"
   admin_state_up      = true
-  external_network_id = "252e901e-cf07-4b47-b243-f6cd11d4df96"
+  external_network_id = "45d3d1ac-f2a8-4b69-85c0-e8cbfc7f3552"
 }
 
 # Network Resource Creation
 resource "openstack_networking_network_v2" "network_1" {
   name              = "network_1"
   admin_state_up    = "true"
-  network_type      = "vlan"
-  physical_network  =  "physint"
-  segmentation_id   =  "220"
 }
 # Creating Subnet for Network
 resource "openstack_networking_subnet_v2" "subnet_1" {
@@ -34,34 +22,43 @@ resource "openstack_networking_subnet_v2" "subnet_1" {
   ip_version = 4
 }
 # Creating Security Group for Network
-resource "openstack_compute_secgroup_v2" "secgroup_1" {
-  name        = "secgroup_1"
-  description = "a security group"
-
-  rule {
-    from_port   = 22
-    to_port     = 22
-    ip_protocol = "tcp"
-    cidr        = "0.0.0.0/0"
-  }
-}
+#resource "openstack_compute_secgroup_v2" "secgroup_1" {
+#  name        = "secgroup_1"
+#  description = "a security group"
+#
+ # rule {
+  #  from_port   = 22
+   # to_port     = 22
+    #ip_protocol = "tcp"
+   # cidr        = "0.0.0.0/0"
+  #}
+#}
 # Creating Port for Network
 resource "openstack_networking_port_v2" "port_1" {
   name               = "port_1"
   network_id         = "${openstack_networking_network_v2.network_1.id}"
   admin_state_up     = "true"
-  security_group_ids = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
+  #security_group_ids = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
 
   fixed_ip {
-    "subnet_id"  = "${openstack_networking_subnet_v2.subnet_1.id}"
-    "ip_address" = "192.168.199.10"
+    subnet_id  =  "${openstack_networking_subnet_v2.subnet_1.id}"
+    ip_address = "192.168.199.10"
   }
 }
+
+#Creating a Keypair
+#resource "openstack_compute_keypair_v2" "ssh-key" {
+#  name = "ssh-key"
+#}
+
+
 # Creating Instance Resource
 resource "openstack_compute_instance_v2" "instance_1" {
   name            = "instance_1"
-  security_groups = ["${openstack_compute_secgroup_v2.secgroup_1.name}"]
-
+  security_groups = ["default"]
+  image_name	= "${var.IMAGE_NAME}"
+  flavor_name	= "${var.FLAVOR_NAME}"
+  key_pair	= "${var.SSH_KEY_NAME}"
   network {
     port = "${openstack_networking_port_v2.port_1.id}"
   }
@@ -71,4 +68,14 @@ resource "openstack_compute_instance_v2" "instance_1" {
 resource "openstack_networking_router_interface_v2" "router_interface_1" {
   router_id = "${openstack_networking_router_v2.router_1.id}"
   subnet_id = "${openstack_networking_subnet_v2.subnet_1.id}"
+}
+
+#Creating Floating IP Pool
+resource "openstack_compute_floatingip_v2" "floatip_1" {
+  pool = "public"
+}
+#Attaching floating ip to Instance
+resource "openstack_compute_floatingip_associate_v2" "floatip_1" {
+  floating_ip = "${openstack_compute_floatingip_v2.floatip_1.address}"
+  instance_id = "${openstack_compute_instance_v2.instance_1.id}"
 }
--- vars.tf
+++ vars.tf
@@ -1,3 +1,6 @@
 variable "OPENSTACK_ADMIN_PASSWORD" {}
 variable "HORIZON_ACCESS_URL"	    {}
-
+variable "SECURITY_GROUP_ID"	    {}
+variable "IMAGE_NAME"		    {}
+variable "FLAVOR_NAME"		    {}
+variable "SSH_KEY_NAME"		    {}
