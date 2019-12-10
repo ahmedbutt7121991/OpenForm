97847517026db4898a934a29f000df3df12931d9
ahmedbutt7121991 <ahmed.butt91@gmail.com>
2019-12-10 00:45:40 -0600

    version changed and syntax from 0.12.2 - 0.12.17

--- provider.tf
+++ provider.tf
@@ -1,9 +1,18 @@
-provider "openstack"{   
-  user_name                     = "admin"
-  tenant_name                   = "admin"
-  password                      = "${var.OPENSTACK_ADMIN_PASSWORD}"
-  auth_url                      = "${var.HORIZON_ACCESS_URL}"
+provider "openstack"{
+  user_name                     = var.ADMIN_USERNAME
+  tenant_name                   = var.ADMIN_TENANTNAME
+  password                      = var.OPENSTACK_ADMIN_PASSWORD
+  auth_url                      = var.HORIZON_ACCESS_URL
   user_domain_name              = "Default"
   project_domain_name           = "Default"
   disable_no_cache_header       = true
 }
+//provider "openstack"{
+//  user_domain_name              = "Default"
+//  project_domain_name           = "Default"
+//  alias                         = "sanity"
+//  user_name                     = "${var.SANITY_USERNAME}"
+//  tenant_name                   = "${var.SANITY_TENANTNAME}"
+//  password                      = "${var.OPENSTACK_SANITY_PASSWORD}"
+//  auth_url                      = "${var.HORIZON_ACCESS_URL}"
+//}
--- vars.tf
+++ vars.tf
@@ -1,4 +1,21 @@
-variable "OPENSTACK_ADMIN_PASSWORD" {}
+variable "ADMIN_USERNAME" {
+  default = "admin"
+}
+variable "ADMIN_TENANTNAME" {
+  default = "admin"
+}
+variable "OPENSTACK_ADMIN_PASSWORD" {
+  default = "wnny7DPCPEYTGsnPQQTCBPkuN"
+}
+variable "SANITY_USERNAME" {
+  default = "sanity_user_1"
+}
+variable "SANITY_TENANTNAME" {
+  default = "sanity_project_1"
+}
+variable "OPENSTACK_SANITY_PASSWORD" {
+  default = "s@n!ty"
+}
 variable "HORIZON_ACCESS_URL"	    {}
 variable "SECURITY_GROUP_ID"	    {}
 variable "IMAGE_NAME"		    {}
@@ -6,15 +23,17 @@ variable "FLAVOR_NAME"		    {}
 variable "SSH_KEY_NAME"		    {}
 variable "SSH_USER_NAME"	    {}
 variable "SSH_KEY_FILE"		    {}
-variable "AVAILABILITY_ZONES" {
-  default = "compute"
-}
-variable "ZONE" {
-  type = "map"
-  default = {
-    compute = "nova"
-    compute0 = "nova0"
-    compute1 = "nova1"
-    compute2 = "nova2"
-  }
-}
+//variable "AVAILABILITY_ZONES" {
+//  default = "compute"
+//}
+//variable "ZONE" {
+//  type = "map"
+//  default = {
+//    compute = "nova"
+//    compute0 = "nova0"
+//    compute1 = "nova1"
+//    compute2 = "nova2"
+//  }
+//}
+
+data "openstack_compute_availability_zones_v2" "zones" {}
